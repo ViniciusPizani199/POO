4c2ca4184f175ac4bf70847090737d9393221cb3
ViniciusPizani199 <vipizaniol@gmail.com>
2025-09-01 09:54:37 -0300

    add simulação

--- sistema_radar/SImulação.java
+++ sistema_radar/SImulação.java
@@ -1,5 +1,27 @@
 package sistema_radar;
 
 public class SImulação {
-    
+    public static void main(String[] args) {
+        Carro opala = new Carro();
+        opala.ano = 76;
+        opala.modelo = "Comodoro";
+        opala.placa = "ACRL300";
+        opala.velocidade = 0;
+
+        Radar radar = new Radar();
+        radar.localização = "Pistão Sul";
+        radar.limiteVelocidade = 60;
+
+        radar.avaliarVelocidade(opala);
+
+        opala.acelerar(); // 10
+        opala.acelerar();
+        opala.acelerar();
+        opala.acelerar();
+        opala.acelerar();
+        opala.acelerar();
+        opala.acelerar(); // 70
+
+        radar.avaliarVelocidade(opala);
+    }
 }
