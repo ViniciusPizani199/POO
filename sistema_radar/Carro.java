cead2e7eab64b1e28086e051eb71b9e05320185e
ViniciusPizani199 <vipizaniol@gmail.com>
2025-09-08 10:12:52 -0300

    melhoria

--- sistema_radar/Carro.java
+++ sistema_radar/Carro.java
@@ -15,6 +15,24 @@ public Carro(String placa, Integer velocidade, Integer ano, String modelo){
 public Integer getVelocidade(){
     return this.velocidade;
 }
+public String getPlaca(){
+    return this.placa;
+}
+public Integer getAno(){
+    return this.ano;
+}
+public String getModelo(){
+    return this.modelo;
+}
+public void setPlaca(String placa){
+    this.placa = placa;
+}
+public void setAno(Integer ano){
+    this.ano = ano;
+}
+public void setModelo(String modelo){
+    this.modelo = modelo;
+}
 public void setVelocidade(Integer velocidade){
     if(velocidade >= 0){
         this.velocidade = velocidade;
